68c7df557301e2bc05f4db0647bb032fbbceafe2
LamHoang-01 <nhlam011019@gmail.com>
2021-10-26 15:20:28 +0700

    Add Button again

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,58 +1,38 @@
 <template>
     <div v-if="text == 'Confirm'">
-      <button 
-        class="btn btn-confirm" 
-        :style="{ width: width, height: height }"
-      >{{text}}</button>
+      <md-button :style="[style, styleConfirm]">{{text}}</md-button>
     </div>
 
     <div v-else-if="text == 'Cancel'">
-      <button 
-        class="btn btn-cancel" 
-        :style="{ width: width, height: height }"
-      >{{text}}</button>
+      <md-button :style="[style, styleConfirm, styleCancel]">{{text}}</md-button>
     </div>
 
     <div v-else-if="text == 'Edit'">
-      <button 
-        class="btn btn-edit" 
-        :style="{ width: width, height: height }"
-      >{{text}}</button>
+      <md-button :style="[style, styleEdit]">{{text}}</md-button>
     </div>
 
     <div v-else-if="text == 'Delete'">
-      <button 
-        class="btn btn-delete" 
-        :style="{ width: width, height: height }"
-      >{{text}}</button>
+      <md-button :style="[style, styleEdit, styleDelete]">{{text}}</md-button>
     </div>
 
     <div v-else-if="text == 'Create Account'">
-      <button 
-        class="btn btn-createAcc" 
-        :style="{ width: width, height: height }"
-      >{{text}}</button>
+      <md-button :style="[style, styleCreate]">{{text}}</md-button>
     </div>
 
     <div v-else-if="text == 'Add Product'">
-      <button 
-        class="btn btn-add" 
-        :style="{ width: width, height: height }"
-      >{{text}}</button>
+      <md-button :style="[style, styleAddPd]">{{text}}</md-button>
+    </div>
+
+    <div v-else-if="text == 'Add New Picture'">
+      <md-button :style="[style, styleAddPt]">{{text}}</md-button>
     </div>
 
     <div v-else-if="text == 'Book Your Table' || text == 'Book My Table' || text == 'Explore More'">
-      <button 
-        class="btn btn-book" 
-        :style="{ width: width, height: height }"
-      >{{text}}</button>
+      <md-button :style="[style, styleBook]">{{text}}</md-button>
     </div>
 
     <div v-else>
-      <button 
-        class="btn" 
-        :style="{ width: width, height: height }"
-      >{{text}}</button>
+      <md-button :style="style">{{text}}</md-button>
     </div>
 </template>
 
@@ -63,65 +43,50 @@ export default {
         text: String,
         width: String,
         height: String
+    },
+    data() {
+      return {
+        style: {
+          width: this.width, 
+          height: this.height, 
+          color: 'white', 
+          background: '#e1651f', 
+          borderRadius: '5px' , 
+          fontSize: '18px',
+          boxShadow: '0px 10px 30px rgba(64, 191, 255, 0.24)',
+        },
+        styleConfirm: {
+          borderRadius: '6px',
+        },
+        styleCancel: {
+          color: '#c4c4c4',
+          background: 'transparent',
+          border: '1px solid #c4c4c4',
+        },
+        styleEdit: {
+          borderRadius: '50px',
+          fontSize: '14px',
+        },
+        styleDelete: {
+          background: 'black',
+        },
+        styleCreate: {
+          borderRadius: '0px',
+          fontSize: '14px',
+        },
+        styleAddPd: {
+          borderRadius: '15px',
+        },
+        styleAddPt: {
+          color: '#e1651f',
+          background: 'white',
+          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+        },
+        styleBook: {
+          borderRadius: '39px',
+          fontSize: '14px',
+        }
+      }
     }
 }
 </script>
-
-<style>
-    .btn {
-        border: none;
-        color: white;
-        text-align: center;
-        text-decoration: none;
-        font-size: 18px;
-        cursor: pointer;
-        font-weight: 500;
-        letter-spacing: 0.5px;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        background-color: #e1651f;
-        border-radius: 5px;
-    }
-    .btn:hover {
-        transition: all .3s ease-in-out;
-        background-color: rgb(231, 120, 56);
-        box-shadow: 0px 5px 5px rgba(236, 129, 40, 0.24);
-    }
-    .btn-confirm, .btn-cancel {
-        border-radius: 6px;
-        box-shadow: 0px 0px 0px;
-    }
-    .btn-cancel {
-        color: #c4c4c4;
-        background-color: white;
-        border: 1px solid #c4c4c4;
-    }
-    .btn-cancel:hover {
-        transition: all .3s ease-out;
-        color: white ;
-        background-color: #dad8d8;
-        box-shadow: 0px 5px 5px rgba(180, 177, 175, 0.24);
-    }
-    .btn-edit, .btn-delete {
-        border-radius: 50px;
-        font-size: 14px;
-    }
-    .btn-delete {
-        background-color: black;
-    }
-    .btn-delete:hover {
-        transition: all .3s ease-in-out;
-        background-color: rgb(51, 47, 47);
-        box-shadow: 0px 5px 5px rgba(78, 74, 70, 0.24);
-    }
-    .btn-createAcc {
-        font-size: 14px;
-        border-radius: 0;
-    }
-    .btn-add {
-        border-radius: 15px;
-    }
-    .btn-book {
-        border-radius: 39px;
-        font-size: 14px;
-    }
-</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,13 +3,17 @@
     <div id="home-page">
       Home Page
     </div>
-    <div style="display: flex">
+    <div>
       <Button text="Create Account" :width="'351px'" :height="'40px'"/>
       <Button text="Confirm" :width="'100px'" :height="'40px'"/>
       <Button text="Cancel" :width="'100px'" :height="'40px'"/>
       <Button text="Edit" :width="'96px'" :height="'27px'"/>
       <Button text="Delete" :width="'96px'" :height="'27px'"/>
       <Button text="Send" :width="'100px'" :height="'64px'"/>
+      <Button text="Add Product" :width="'222px'" :height="'51px'"/>
+      <Button text="Book My Table" :width="'169px'" :height="'53px'"/>
+      <Button text="Add New Picture" :width="'343px'" :height="'64px'"/>
+      <Button text="Login" :width="'257px'" :height="'64px'"/>
     </div>
   </div>
 </template>
@@ -24,7 +28,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-</style>
