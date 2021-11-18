7e58b1d19f3baf7e3ce2aef04fdc8c721eb8b009
LamHoang-01 <nhlam011019@gmail.com>
2021-11-18 22:08:07 +0700

    Fix size of old page

--- src/components/Dish.vue
+++ src/components/Dish.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-card class="mx-5 my-10" width="258" height="370">
+  <v-card class="mx-8 my-10" width="330" height="400">
     <div>
-        <v-img height="208" :src=dish.image />
+        <v-img height="220" :src=dish.image />
     </div>
     <div class="dish-container">
         <p class="dish-title">{{dish.name}}</p>
@@ -20,25 +20,21 @@ export default {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Oleo+Script+Swash+Caps&display=swap');
+@import url("//db.onlinewebfonts.com/c/c8e8fcd5d85d9b587022fe0a79809b73?family=Friz+Quadrata");
     .dish-container {
-        padding: 2% 6% 0% 6%;
+        padding: 4% 8% 0% 8%;
     }
     .dish-title, .dish-description, .dish-price {
         margin: 3px 0px;
     }
     .dish-title {
         font-size: 24px;
-        font-family: Oleo Script Swash Caps;
+        /* font-family: Oleo Script Swash Caps; */
+        font-weight: 500;
     }
     .dish-description {
         font-size: 14px;
         color: #6a6a6a;
-        letter-spacing: 0.5px;
-    }
-    .dish-price {
-        font-weight: bold;
-        font-size: 20px;
         letter-spacing: 0.5px;
     }
 </style>
